2de9952bbf91cdbd98b064c0a0d459ab92c7b2e2
Shriteen <86562935+Shriteen@users.noreply.github.com>
2022-02-12 21:04:32 +0530

    Changed type of positions to float

--- sprite.cpp
+++ sprite.cpp
@@ -1,23 +1,4 @@
-#include<map>
-#include<ncurses.h>
-using namespace std;
-
-/*	Sprite class is to be used as base class
- *  for objects having physical appearance	*/
-class sprite
-{
-	protected:
-		int x_pos;									//position x-coordinate
-		int y_pos;									//position y-coordinate
-		map< pair<int,int> , char > pixels;			//map which represents pixels as characters as value and
-													//keys are pairs of coordinates relative to position in (y,x) form
-		WINDOW *win;								//window in which sprite is drawn
-	public:
-		virtual void draw();						//draw the sprite in the virtual window (Not visible until refresh is called)
-		void clean();								//fills the position with spaces effectively erasing the sprite
-		bool touches(sprite &other);				//Return true if touching other sprite
-		
-};
+#include "sprite.h"
 
 void sprite::draw()
 {
